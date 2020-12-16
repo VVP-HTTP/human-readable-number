edcaaaac9257d413d01eb4cc28355a0093baa195
Paul Iambaev <slushau.vnimatelno@yandex.ru>
2020-12-16 19:28:00 +0500

    refactor

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    if (number === 0) {
+        return "zero";
+    }
+    const hundred = (number / 100) ^ 0;
+    const test = number % 100;
+    const d = ((test / 10) ^ 0) * 10;
+    const units = test <= 19 ? test : test % 10;
+    const numbers = {
+        0: "",
+        1: "one",
+        2: "two",
+        3: "three",
+        4: "four",
+        5: "five",
+        6: "six",
+        7: "seven",
+        8: "eight",
+        9: "nine",
+        10: "ten",
+        11: "eleven",
+        12: "twelve",
+        13: "thirteen",
+        14: "fourteen",
+        15: "fifteen",
+        16: "sixteen",
+        17: "seventeen",
+        18: "eighteen",
+        19: "nineteen",
+        20: "twenty",
+        30: "thirty",
+        40: "forty",
+        50: "fifty",
+        60: "sixty",
+        70: "seventy",
+        80: "eighty",
+        90: "ninety",
+    };
+
+    return `${hundred ? numbers[hundred] + " hundred" : ""} ${
+        test > 19
+            ? numbers[d] + (units <= 9 ? ` ${numbers[units]}` : "")
+            : numbers[units]
+    }`.trim();
+};
